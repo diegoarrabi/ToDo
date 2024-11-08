5047591382ce049ed64caeec31a5c7fa172a65c5
diego ibarra <diegoarrabi1@gmail.com>
2024-11-07 23:11:01 -0500

    remove all resources from public repo

--- resources/.backup/ToDo_1004/resources/ToDoLauncher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-export PYENV_ROOT="$XDG_CONFIG_HOME/pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-cd /Users/diegoibarra/Developer/1_myProjects/Apps/ToDo
-
-python3 makeTasks.py
